e0c99e23f51aba70731c4f168f6a607e13720c3b
nocturnal <lonelykida7@yahoo.com>
2026-06-09 22:31:43 +0700

    Minor fix

--- app/admin/exercises/page.tsx
+++ app/admin/exercises/page.tsx
@@ -1079,8 +1079,8 @@ export default function AdminExerciseBlueprints() {
                   className="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-700 text-gray-900 dark:text-white"
                 >
                   <option value="basic">Cơ bản</option>
-                  <option value="intermediate">Trung bình</option>
-                  <option value="advanced">Nâng cao</option>
+                  <option value="intermediate">Thông hiểu</option>
+                  <option value="advanced">Vận dụng cao</option>
                 </select>
               </div>
 
